5a44a365d709808cdeaf83243531c6c75fb97396
GargKrishna24 <gargkrishna@888@gmail.com>
2025-07-09 22:01:07 +0530

    Modify : add Navbar in components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import MyProfile from './pages/MyProfile'
-import MyAppointment from './pages/MyAppointment'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Login from './pages/Login'
-import Appointment from './pages/Appointment'
-import Navbar from'./components/Navbar'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import MyProfile from "./pages/MyProfile";
+import MyAppointment from "./pages/MyAppointment";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
+import Doctor from "./pages/Doctor";
 
 function App() {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
+    <div className="mx-4 sm:mx-[10%]">
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/Doctors' element={<Doctors />}/>
-        <Route path='/Doctors/:speciality' element={<Doctors />}/>
-        <Route path='/Login' element={<Login />}/>
-        <Route path='/About' element={<About />}/>
-        <Route path='/Contact' element={<Contact />}/>
-        <Route path='/my-profile' element={<MyProfile />}/>
-        <Route path='/my-appointments' element={<MyAppointments />}/>
-        <Route path='/my-appointments/:docId' element={<Appointment />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Doctor" element={<Doctor />} />
+        <Route path="/Doctor/:speciality" element={<Doctor />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointment />} />
+        <Route path="/my-appointments/:docId" element={<Appointment />} />
       </Routes>
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { assets } from '../assets/assets.js'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b-gray-400'>
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
+        <NavLink>
+          <li className="py-1">HOME</li>
+          <hr className = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+        </NavLink>
+        <NavLink>
+          <li className="py-1">All Doctors</li>
+          <hr  className = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+        </NavLink>
+        <NavLink>
+          <li className="py-1">ABOUT</li>
+          <hr  className = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+        </NavLink>
+        <NavLink>
+          <li className="py-1">Contact</li>
+          <hr  className = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+        </NavLink>
+      </ul>
+      <div>
+        <button>Create account </button>
+      </div>
+    </div>
   )
 }
 
