a4efb0716337156e971b5c2f0fc816398bb33716
GargKrishna24 <gargkrishna@888@gmail.com>
2025-07-09 21:17:49 +0530

    feat : add Navbar in Components Folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,12 @@ import Contact from './pages/Contact'
 import About from './pages/About'
 import Login from './pages/Login'
 import Appointment from './pages/Appointment'
+import Navbar from'./components/Navbar'
 
 function App() {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/Doctors' element={<Doctors />}/>
